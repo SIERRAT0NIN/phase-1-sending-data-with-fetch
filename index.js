4fa2c01770005662c5135a9f856bc1b0523d0448
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-18 21:29:10 -0700

    fetching data lab done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+const url = "http://localhost:3000/users";
+const submitData = (name, email) => {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((object) => (document.body.innerHTML = object["id"]))
+    .catch((error) => (document.body.innerHTML = error.message));
+};
+
+submitData("alberto", "albeto@sierra.com");
+submitData("antonio", "antonio@sierra.com");
